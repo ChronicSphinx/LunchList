14cc02014c516a7b473b591317e2c785601a2106
Christopher Budge <Christopher.Budge@mtctrains.com>
2012-09-06 14:40:11 -0600

    tut 7 complete

--- src/com/example/lunchlist/MainActivity.java
+++ src/com/example/lunchlist/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,6 +20,7 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends TabActivity 
 {
@@ -28,6 +30,7 @@ public class MainActivity extends TabActivity
   EditText address = null;
   RadioGroup types = null;
   EditText notes = null;
+  Restaurant current = null;
   
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,36 +65,52 @@ public class MainActivity extends TabActivity
         return (super.onCreateOptionsMenu(menu));
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	    if (item.getItemId()==R.id.toast) 
+	    {
+		    String message="No restaurant selected";
+		    if (current!=null) 
+		    {
+		    	message=current.getNotes();
+		    }
+		    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+		    return(true);
+	    }
+	    return(super.onOptionsItemSelected(item));
+    }
+    
     private View.OnClickListener onSave = new View.OnClickListener()
     {
       
       @Override
       public void onClick(View v) 
       {
-        Restaurant r = new Restaurant();
-        EditText name = (EditText)findViewById(R.id.name);
-        EditText address = (EditText)findViewById(R.id.addr);
-        r.setName(name.getText().toString());
-        r.setAddress(address.getText().toString());
-        
+        current = new Restaurant();
+//        EditText name = (EditText)findViewById(R.id.name);
+//        EditText address = (EditText)findViewById(R.id.addr);
+//        EditText notes = (EditText)findViewById(R.id.notes);
+        current.setName(name.getText().toString());
+        current.setAddress(address.getText().toString());
+        current.setNotes(notes.getText().toString());
         RadioGroup types = (RadioGroup)findViewById(R.id.types);
         
         switch (types.getCheckedRadioButtonId())
         {
           case R.id.sit_down:
-            r.setType("sit_down");
+            current.setType("sit_down");
             break;
             
           case R.id.take_out:
-            r.setType("take_out");
+            current.setType("take_out");
             break;
           
           case R.id.delivery:
-            r.setType("delivery");
+            current.setType("delivery");
             break;
             
         }
-        adapter.add(r);
+        adapter.add(current);
       }
     };
     
@@ -99,14 +118,14 @@ public class MainActivity extends TabActivity
     {
     		public void onItemClick(AdapterView<?> parent,View view, int position,long id) 
     		{
-    			Restaurant r = model.get(position);
-    			name.setText(r.getName());
-    			address.setText(r.getAddress());
-    			notes.setText(r.getNotes());
-    			if (r.getType().equals("sit_down")) {
+    			current = model.get(position);
+    			name.setText(current.getName());
+    			address.setText(current.getAddress());
+    			notes.setText(current.getNotes());
+    			if (current.getType().equals("sit_down")) {
     				types.check(R.id.sit_down);
 				}
-				else if (r.getType().equals("take_out")) {
+				else if (current.getType().equals("take_out")) {
 					types.check(R.id.take_out);
 				}
 				else {
