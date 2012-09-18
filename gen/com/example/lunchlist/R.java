897f80a0d19dc9e11a2a126c45169af5f707ed56
Christopher Budge <Christopher.Budge@mtctrains.com>
2012-09-18 14:49:28 -0600

    start of tut 15

--- gen/com/example/lunchlist/R.java
+++ gen/com/example/lunchlist/R.java
@@ -21,26 +21,28 @@ public final class R {
         public static final int ic_action_search=0x7f020003;
         public static final int ic_launcher=0x7f020004;
         public static final int ic_menu_add=0x7f020005;
-        public static final int ic_menu_preferences=0x7f020006;
-        public static final int list=0x7f020007;
-        public static final int restaurant=0x7f020008;
-        public static final int run=0x7f020009;
-        public static final int toast=0x7f02000a;
+        public static final int ic_menu_friendslist=0x7f020006;
+        public static final int ic_menu_preferences=0x7f020007;
+        public static final int list=0x7f020008;
+        public static final int restaurant=0x7f020009;
+        public static final int run=0x7f02000a;
+        public static final int toast=0x7f02000b;
     }
     public static final class id {
-        public static final int add=0x7f09000c;
+        public static final int add=0x7f09000d;
         public static final int addr=0x7f090001;
-        public static final int address=0x7f09000a;
+        public static final int address=0x7f09000b;
         public static final int delivery=0x7f090005;
-        public static final int icon=0x7f090008;
-        public static final int menu_settings=0x7f09000b;
+        public static final int feed=0x7f090007;
+        public static final int icon=0x7f090009;
+        public static final int menu_settings=0x7f09000c;
         public static final int name=0x7f090000;
         public static final int notes=0x7f090006;
-        public static final int prefs=0x7f09000d;
-        public static final int save=0x7f090007;
+        public static final int prefs=0x7f09000e;
+        public static final int save=0x7f090008;
         public static final int sit_down=0x7f090004;
         public static final int take_out=0x7f090003;
-        public static final int title=0x7f090009;
+        public static final int title=0x7f09000a;
         public static final int types=0x7f090002;
     }
     public static final class layout {
@@ -50,7 +52,8 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
-        public static final int option=0x7f080001;
+        public static final int details_option=0x7f080001;
+        public static final int option=0x7f080002;
     }
     public static final class string {
         public static final int address=0x7f060005;
--- src/com/example/lunchlist/DetailForm.java
+++ src/com/example/lunchlist/DetailForm.java
@@ -1,21 +1,29 @@
 package com.example.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity
 {
 	EditText name = null;
 	EditText address = null;
 	EditText notes = null;
+	EditText feed = null;
 	RadioGroup types = null;
 	RestaurantHelper helper = null;
-	String restaurantId = null;
+	String restaurantId = null;	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -26,6 +34,7 @@ public class DetailForm extends Activity
         address = (EditText)findViewById(R.id.addr);
         types = (RadioGroup)findViewById(R.id.types);
         notes = (EditText)findViewById(R.id.notes);
+        feed = (EditText)findViewById(R.id.feed);
         Button save =(Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
         restaurantId = getIntent().getStringExtra(MainActivity.ID_EXTRA);
@@ -41,6 +50,62 @@ public class DetailForm extends Activity
 		super.onDestroy();
 		
 		helper.close();
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle state)
+	{
+		super.onSaveInstanceState(state);
+		
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state)
+	{
+		super.onRestoreInstanceState(state);
+		
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
+		
+		return (super.onCreateOptionsMenu(menu));
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		if (item.getItemId()==R.id.feed) 
+		{
+			if (isNetworkAvailable()) 
+			{
+				Intent i=new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			}
+			else 
+			{
+				Toast.makeText(this, "Sorry, the Internet is not available",Toast.LENGTH_LONG).show();
+			}
+			return(true);
+		}
+		return(super.onOptionsItemSelected(item));
+	}
+	private boolean isNetworkAvailable() 
+	{
+		ConnectivityManager cm=(ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info=cm.getActiveNetworkInfo();
+		return(info!=null);
 	}
 	
 	private View.OnClickListener onSave=new View.OnClickListener() {
@@ -62,11 +127,11 @@ public class DetailForm extends Activity
 			
 			if(restaurantId == null)
 			{
-				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
 			}
 			else
 			{
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString(),feed.getText().toString());
 			}
 			finish();
 		}
@@ -79,6 +144,7 @@ public class DetailForm extends Activity
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		if (helper.getType(c).equals("sit_down")) 
 		{
 			types.check(R.id.sit_down);
